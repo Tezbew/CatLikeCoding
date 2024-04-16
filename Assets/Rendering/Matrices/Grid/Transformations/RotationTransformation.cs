95da2ad6084a49ac886da7d82510c4366ae1bba4
Artem Venger <artemvengertilshift@gmail.com>
2024-04-16 21:36:55 +0700

    Used matrices for the rotation

--- Assets/Rendering/Matrices/Grid/Transformations/RotationTransformation.cs
+++ Assets/Rendering/Matrices/Grid/Transformations/RotationTransformation.cs
@@ -2,20 +2,39 @@
 
 namespace Rendering.Matrices
 {
-    public class RotationTransformation : Transformation {
-
+    public class RotationTransformation : Transformation
+    {
         public Vector3 Rotation;
 
-        public override Vector3 Apply (Vector3 point) {
+        public override Vector3 Apply (Vector3 point)
+        {
+            var radX = Rotation.x * Mathf.Deg2Rad;
+            var radY = Rotation.y * Mathf.Deg2Rad;
             var radZ = Rotation.z * Mathf.Deg2Rad;
+            var sinX = Mathf.Sin(radX);
+            var cosX = Mathf.Cos(radX);
+            var sinY = Mathf.Sin(radY);
+            var cosY = Mathf.Cos(radY);
             var sinZ = Mathf.Sin(radZ);
             var cosZ = Mathf.Cos(radZ);
 
-            return new Vector3(
-                point.x * cosZ - point.y * sinZ,
-                point.x * sinZ + point.y * cosZ,
-                point.z
+            var xAxis = new Vector3(
+                cosY * cosZ,
+                cosX * sinZ + sinX * sinY * cosZ,
+                sinX * sinZ - cosX * sinY * cosZ
             );
+            var yAxis = new Vector3(
+                -cosY * sinZ,
+                cosX * cosZ - sinX * sinY * sinZ,
+                sinX * cosZ + cosX * sinY * sinZ
+            );
+            var zAxis = new Vector3(
+                sinY,
+                -sinX * cosY,
+                cosX * cosY
+            );
+
+            return xAxis * point.x + yAxis * point.y + zAxis * point.z;
         }
     }
 }
