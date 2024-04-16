47ea05f8fdfe8413ba66564d3633b9dc715d171b
Artem Venger <artemvengertilshift@gmail.com>
2024-04-16 21:07:00 +0700

    Created the scale and rotation components

---
+++ Assets/Rendering/Matrices/Grid/Transformations/RotationTransformation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Rendering.Matrices
+{
+    public class RotationTransformation : Transformation {
+
+        public Vector3 Rotation;
+
+        public override Vector3 Apply (Vector3 point) {
+            var radZ = Rotation.z * Mathf.Deg2Rad;
+            var sinZ = Mathf.Sin(radZ);
+            var cosZ = Mathf.Cos(radZ);
+
+            return new Vector3(
+                point.x * cosZ - point.y * sinZ,
+                point.x * sinZ + point.y * cosZ,
+                point.z
+            );
+        }
+    }
+}
